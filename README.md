7940f4681ab964acbd5f1453bb409d62819e91c3
Matt Cook <mcook@gettilled.com>
2021-04-14 15:37:15 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# simple-payment-example
+# Get started
+
+- clone the project
+- `npm install`
+
+# Create a sandbox account and add your configuration values
+
+- [Register a sandbox account](https://sandbox-app.tilled.com/register)
+- Create a secret and publishable api key
+- Update line 138 of index.html with your publishable API Key
+- Update line 7 of app.js with your secret API key
+- Add an Account [here](https://sandbox-app.tilled.com/connected-accounts)
+- Note the Account ID and update line 136 of index.html with the Account ID
+
+# Process your first payment
+- Open index.html in your browser, enter `4037111111000000` as the test card number with a valid expiration date and `123` as the CVV Code
+- Click Pay
+- Look in the browser's developer console to see payment intent creation logs
+- Go [here](https://sandbox-app.tilled.com/payments) to see your payment
+
+# What's Next?
+[API Docs](api.tilled.com/docs)
+
+You can try out attaching payment methods to customers, adding metadata, including platform fees on payment intents and much, much more via the Tilled API.
---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const axios = require('axios')
+const app = express()
+const path = require('path')
+const port = process.env.port || 5000
+
+const tilledSecretApiKey = 'sk_SECRET_KEY_HERE'
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+
+app.get('/secret/:id', (req, res) => {
+  const tilledAccountId = req.params.id
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: 'Bearer ' + tilledSecretApiKey,
+    'Tilled-Account': tilledAccountId,
+  }
+
+  axios.post('https://sandbox-api.tilled.com/v1/payment-intents',
+    {
+      amount: 500,
+      currency: 'usd',
+      payment_method_types: ['card'],
+    },
+    {
+      headers: headers,
+    })
+    .then((response) => {
+      console.log('Successfully created payment intent:')
+      console.log(response.data)
+      res.json({ client_secret: response.data.client_secret })
+    })
+    .catch((error) => {
+      let errorMsg = 'Unable to create and return paymentIntent'
+
+      if (error.response) {
+      // Request made and server responded
+        console.log(error.response.data)
+        errorMsg = error.response.data.message
+      } else if (error.request) {
+      // The request was made but no response was received
+        console.log(error.request)
+      } else {
+      // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message)
+      }
+
+      res.status(400).send({ message: errorMsg })
+    })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
